0d7b6a569a08a642f9b20adfb78ed62b3f396c9e
Huyen Pham <thanhhuyen2995@gmail.com>
2019-06-14 21:44:47 -0700

    created README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+### Diary Journal
+* Let users create blog posts and show the preview (short version) in homepage.
+* Using Ejs for templating.
--- app.js
+++ app.js
@@ -74,9 +74,6 @@ app.get("/posts/:post", function(req,res) {
 });
 
 
-
-
-
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
